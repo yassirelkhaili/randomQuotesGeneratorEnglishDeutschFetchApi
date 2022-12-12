78f5136319d7a8e4acafc19827c041f940dd13a6
yassirelkhaili <elkhailiyassir@gmail.com>
2022-12-12 19:53:40 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [joke, setjoke] = useState([]);
+  useEffect(() => {
+    async function fetchJokes() {
+      const response = await fetch(
+        "https://v2.jokeapi.dev/joke/Programming,Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
+      );
+      const data = await response.json();
+      setjoke(data);
+    }
+    fetchJokes();
+  }, []);
+
+  const getNewJoke = () => {
+    const myFrom = document.getElementById("form");
+    if (myFrom.value === "english") {
+      async function fetchJokes() {
+        const response = await fetch(
+          "https://v2.jokeapi.dev/joke/Programming,Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
+        );
+        const data = await response.json();
+        setjoke(data);
+      }
+      fetchJokes();
+    } else if (myFrom.value === "deutsch") {
+      async function fetchJokes() {
+        const response = await fetch(
+          "https://v2.jokeapi.dev/joke/Programming,Pun?lang=de&blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
+        );
+        const data = await response.json();
+        setjoke(data);
+      }
+      fetchJokes();
+    } else {
+      setjoke(
+          {
+            joke: "Select Language"
+          }
+      );
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper container" id="quote-box">
+      <div id="text">
+        {joke.joke ? (
+          <>
+            <p id="joke">{joke.joke}</p>
+          </>
+        ) : (
+          <>
+            <h4>Loading</h4>
+          </>
+        )}
+      </div>
+      <div id="author">
+        {joke.category ? (
+          <>
+            <p id="category">Category : {joke.category}</p>
+          </>
+        ) : (
+          <>
+            <h4>Loading</h4>
+          </>
+        )}
+      </div>
+      <div className="row">
+        <div className="col">
+          <button
+            id="new-quote"
+            className="btn btn-primary"
+            onClick={getNewJoke}
+          >
+            New joke
+          </button>
+        </div>
+        <div className="col">
+          <button className="btn btn-primary">
+            <a id="tweet-quote" href="twitter.com/intent/tweet">
+            </a>
+            Tweet joke
+          </button>
+        </div>
+        <div className="col-5">
+          <select className="form-select" id="form">
+            <option selected>Sprache/Language</option>
+            <option value="english">English</option>
+            <option value="deutsch">Deutsch</option>
+          </select>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
